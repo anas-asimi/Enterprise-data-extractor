d56f1ab3a76354a7d087c9351aadb7eee5299838
Anas Asimi <anas2020cr7@gmail.com>
2023-07-29 22:15:55 +0100

    add executablePath property to browser configuration

--- utils/config.js
+++ utils/config.js
@@ -1,14 +1,21 @@
 import cities from '../json files/cities.json' assert {type: 'json'};
 import keywords from '../json files/keywords.json' assert {type: 'json'};
-// import dotenv from 'dotenv';
-// dotenv.config();
+import { getEdgePath } from 'edge-paths';
 
 export const browserConfig = {
-    headless: false,
-    devtools: true,
-    defaultViewport: null,
-    ignoreDefaultArgs: ["--enable-automation"],
-    args: ["--start-maximized", '--lang=bn-BD,bn', '--test-type', '--disable-infobars','--disable-features=DarkMode']
+	executablePath: getEdgePath(),
+	headless: false,
+	devtools: true,
+	defaultViewport: null,
+	ignoreDefaultArgs: ["--enable-automation"],
+	args: [
+		"--start-maximized",
+		"--lang=bn-BD,bn",
+		"--test-type",
+		// "--disable-infobars",
+		// "--disable-features=DarkMode",
+		"--guest",
+	],
 };
 
 export const jsonFilePath = './json files/collectedData.json'
